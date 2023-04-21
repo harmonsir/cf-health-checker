0d1f56ce39441baaad35e53d81b5e6714c228e49
harmonsir <127877986+harmonsir@users.noreply.github.com>
2023-04-22 07:53:30 +0800

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,25 +1,45 @@
-export default {
-    async fetch(request, env) {
-        const Hosts = {
-          
-        };
+addEventListener("scheduled", (event) => {
+    event.waitUntil(handleFetch(event));
+});
 
-        async function gatherResponse(response) {
-            return response.status < 300;
+addEventListener('fetch', event => {
+    event.respondWith(handleFetch(event));
+});
+
+const HostsCacheKey = "HealthCheckerHosts";
+const CacheKey = "HealthChecker";
+const newRequestInit = {
+    method: "GET",
+    headers: {
+        "content-type": "text/html;charset=UTF-8",
+        "user-agent": "cf_worker health_check v0.1"
+    },
+};
+
+async function gatherResponse(response) {
+    return response.status < 300;
+}
+
+async function handleFetch(event) {
+    if (event.type === "fetch") {
+        const content = await KV_MIDDLEWARE.get(CacheKey);
+        if (content) {
+            return new Response(content)
         }
-
-        const init = {
-            headers: {
-                "content-type": "text/html;charset=UTF-8",
-                "user-agent": "cf_worker health_check v0.1"
-            },
-        };
-
-        let result = ""
-        for (let key in Hosts) {
-            let r = await fetch(Hosts[key], init);
-            result += key + " -> " + await gatherResponse(r) + "\n"
-        }
-        return new Response("Health Checks: \n\n" + result)
     }
+    const Hosts = await KV_MIDDLEWARE.get(HostsCacheKey, {type: "json"});
+    let result = {}
+    for (let key in Hosts) {
+        let r = new Request(Hosts[key], newRequestInit);
+        result[key] = await gatherResponse(await fetch(r));
+        // result += key + " -> " + await gatherResponse(r) + "\n"
+    }
+
+    const content = JSON.stringify({
+        "APP": "HealthChecker",
+        "TS": new Date().toLocaleString("zh-Hans-CN", {timeZone: "Asia/Shanghai"}),
+        result
+    })
+    await KV_MIDDLEWARE.put(CacheKey, content);
+    return new Response(content)
 }
